b64be41e52cdd2ad2e6f6aa116fbe4ae73885df4
chenhuayou <chen.hua.you@163.com>
2022-05-04 22:43:33 +0800

    camera okay and wifi okay

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,48 @@
-use std::{thread};
-use log::*;
-use std::{cell::RefCell, env, sync::atomic::*, sync::Arc, thread, time::*};
-//use esp_idf_svc::netif::*;
+use esp_idf_svc::netif::EspNetifStack;
+use esp_idf_svc::sysloop::EspSysLoopStack;
+use std::sync::Arc;
+use esp_idf_svc::nvs::EspDefaultNvs;
+use anyhow::Result;
 use esp_idf_svc::wifi::EspWifi;
+use log::info;
+use embedded_svc::wifi::Wifi;
+use std::env;
+use embedded_svc::wifi::Configuration;
+use embedded_svc::wifi::ClientConfiguration;
+use embedded_svc::wifi::AccessPointConfiguration;
+use std::time::Duration;
+use embedded_svc::wifi::Status;
+use embedded_svc::wifi::ClientStatus;
+use embedded_svc::wifi::ApStatus;
+use embedded_svc::wifi::ClientConnectionStatus;
+use embedded_svc::wifi::ClientIpStatus;
+use embedded_svc::wifi::ApIpStatus;
+use embedded_svc::ipv4;
+use esp_idf_svc::ping::EspPing;
+use embedded_svc::ping::Ping;
+use std::sync::{Condvar, Mutex};
+use esp_idf_svc::httpd::ServerRegistry;
+use esp_idf_svc::httpd::Server;
+use embedded_svc::httpd::Response;
+use embedded_svc::httpd::registry::Registry;
+use std::panic;
+use std::thread;
+use std::ffi::CString;
+use esp_idf_sys::c_types::c_void;
+use core::{marker::PhantomData, ptr};
 
-fn wifi(
+const SSID: &str = env!("RUST_ESP32_STD_DEMO_WIFI_SSID");
+const PASS: &str = env!("RUST_ESP32_STD_DEMO_WIFI_PASS");
+
+
+
+
+fn mywifi(
     netif_stack: Arc<EspNetifStack>,
     sys_loop_stack: Arc<EspSysLoopStack>,
     default_nvs: Arc<EspDefaultNvs>,
 ) -> Result<Box<EspWifi>> {
+    println!("hello -------------------------------------------> wifi?");
     let mut wifi = Box::new(EspWifi::new(netif_stack, sys_loop_stack, default_nvs)?);
 
     info!("Wifi created, about to scan");
@@ -57,17 +91,80 @@ fn wifi(
         ApStatus::Started(ApIpStatus::Done),
     ) = status
     {
-        info!("Wifi connected");
+        println!("Wifi connected");
 
         ping(&ip_settings)?;
     } else {
-        bail!("Unexpected Wifi status: {:?}", status);
+        anyhow::bail!("Unexpected Wifi status: {:?}", status);
     }
 
     Ok(wifi)
 }
 
+fn ping(ip_settings: &ipv4::ClientSettings) -> Result<()> {
+    info!("About to do some pings for {:?}", ip_settings);
 
+    let ping_summary =
+        EspPing::default().ping(ip_settings.subnet.gateway, &Default::default())?;
+    if ping_summary.transmitted != ping_summary.received {
+        anyhow::bail!(
+            "Pinging gateway {} resulted in timeouts",
+            ip_settings.subnet.gateway
+        );
+    }
+
+    info!("Pinging done");
+
+    Ok(())
+}
+
+fn myhttpd(mutex: Arc<(Mutex<Option<u32>>, Condvar)>) -> Result<Server> {
+    let server = ServerRegistry::new()
+        .at("/")
+        .get(|_| Ok("Hello from Rust!".into()))?
+        .at("/foo")
+        .get(|_| anyhow::bail!("Boo, something happened!"))?
+        .at("/bar")
+        .get(|_| {
+            Response::new(403)
+                .status_message("No permissions")
+                .body("You have no permissions to access this page".into())
+                .into()
+        })?
+        .at("/panic")
+        .get(|_| panic!("User requested a panic!"))?;
+
+    server.start(&Default::default())
+}
+
+unsafe extern "C" fn jpg_stream_httpd_handler(r: *mut esp_idf_sys::httpd_req_t) -> esp_idf_sys::esp_err_t{
+    loop{
+        println!("jpg_stream_httpd_handler !!!!!");
+    }
+}
+fn default_configuration(http_port: u16, https_port: u16) -> esp_idf_sys::httpd_config_t {
+    esp_idf_sys::httpd_config_t {
+        task_priority: 5,
+        stack_size: if https_port != 0 { 10240 } else { 4096 },
+        core_id: std::i32::MAX,
+        server_port: http_port,
+        ctrl_port: 32768,
+        max_open_sockets: if https_port != 0 { 4 } else { 7 },
+        max_uri_handlers: 8,
+        max_resp_headers: 8,
+        backlog_conn: 5,
+        lru_purge_enable: https_port != 0,
+        recv_wait_timeout: 5,
+        send_wait_timeout: 5,
+        global_user_ctx: ptr::null_mut(),
+        global_user_ctx_free_fn: None,
+        global_transport_ctx: ptr::null_mut(),
+        global_transport_ctx_free_fn: None,
+        open_fn: None,
+        close_fn: None,
+        uri_match_fn: None,
+    }
+}
 
 fn main() {
     // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
@@ -102,17 +199,50 @@ fn main() {
 
         jpeg_quality : 12, //0-63 lower number means higher quality
         fb_count : 1,       //if more than one, i2s runs in continuous mode. Use only with JPEG
+        fb_location: esp_idf_sys::camera_fb_location_t_CAMERA_FB_IN_DRAM,
+        grab_mode: esp_idf_sys::camera_grab_mode_t_CAMERA_GRAB_LATEST
     };
+    let netif_stack = Arc::new(EspNetifStack::new().unwrap());
+    let sys_loop_stack = Arc::new(EspSysLoopStack::new().unwrap());
+    let default_nvs = Arc::new(EspDefaultNvs::new().unwrap());
+    let mut _wifi = mywifi(
+        netif_stack.clone(),
+        sys_loop_stack.clone(),
+        default_nvs.clone(),
+        ).unwrap();
     if unsafe{esp_idf_sys::esp_camera_init(&camera_config)} != 0{
         println!("camera init failed!");
         return;
+    }else{
+        println!("camera ready! >>>>>>>>>>>>>>>>>>>>>>>>>>>>");
     }
-    let mut num = 0;
-    loop{
-        println!("Taking picture ... {}",num);
-        let fb = unsafe{ esp_idf_sys::esp_camera_fb_get()};
-        println!("Picture taken! Its size was: {} bytes", unsafe{(*fb).len});
-        unsafe{esp_idf_sys::esp_camera_fb_return(fb);} 
-        num += 1;
+    //let mutex = Arc::new((Mutex::new(None), Condvar::new()));
+    //let httpd = myhttpd(mutex.clone()).unwrap();
+
+    let uri_handler_jpg = esp_idf_sys::httpd_uri_t{
+        uri:CString::new("/stream.jpg").unwrap().as_ptr(),
+        method: esp_idf_sys::http_method_HTTP_GET,
+        handler: Some(jpg_stream_httpd_handler),
+        user_ctx: esp_idf_sys::_NULL as *mut _
+    };
+    let server:esp_idf_sys::httpd_handle_t = esp_idf_sys::_NULL as *mut _;
+    let config:esp_idf_sys::httpd_config_t = default_configuration(80, 443);
+    println!("{:?}",config);
+    unsafe{esp_idf_sys::httpd_start(server as *mut *mut c_void, &config)};
+    unsafe{esp_idf_sys::httpd_register_uri_handler(server, &uri_handler_jpg)};
+
+    
+    for s in 0..360 {
+        println!("Shutting down in {} secs", 3 - s);
+        thread::sleep(Duration::from_secs(1));
     }
+
+    println!("Httpd stopped");
+    //loop{
+    //    println!("Taking picture ... {}",num);
+    //    let fb = unsafe{ esp_idf_sys::esp_camera_fb_get()};
+    //    println!("Picture taken! Its size was: {} bytes", unsafe{(*fb).len});
+    //    unsafe{esp_idf_sys::esp_camera_fb_return(fb);} 
+    //    num += 1;
+    //}
 }
